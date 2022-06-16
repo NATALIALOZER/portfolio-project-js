b012790ae6be87f345a9838e42fbcc6e373fb52b
Nataliia Balabukha <70195460+NATALIALOZER@users.noreply.github.com>
2022-06-16 21:31:44 +0300

    added animation

--- js/scroll.js
+++ js/scroll.js
@@ -1,32 +1,59 @@
-document.addEventListener("DOMContentLoaded", function () {
-  var scroller = document.getElementById("view-area");
-  var sectionContainer = document.getElementById("section-container");
-  var lockerContainer = document.getElementById("locker");
-  var lastIndex = 0;
-  var indexArray = [
+$(document).ready(function () {
+  //var scroller = $("#view-area");
+  const sectionContainer = $("#section-container")[0];
+  const lockerContainer = $("#locker")[0];
+  let lastIndex = 0;
+  let indexArray = [
     { x: 0, y: 0, horizontal: false },
     { x: 0, y: 1, horizontal: true },
     { x: 1, y: 1, horizontal: true },
     { x: 2, y: 1, horizontal: false },
     { x: 0, y: 2, horizontal: false },
   ];
-  var maxRows = 3;
-  var maxCols = 2;
+  //const maxRows = 3;
+  //const maxCols = 2;
 
-  var easeInQuad = function (t) {
-    return t * t;
-  };
-  var easeInCubic = function (t) {
+  // const easeInQuad = function (t) {
+  //   return t * t;
+  // };
+
+  const easeInCubic = function (t) {
     return t * t * t;
   };
 
-  var handleScroll = function (e) {
-    var doc = document.documentElement;
-    var top = (window.pageYOffset || doc.scrollTop) - (doc.clientTop || 0);
+  const handleScroll = function (e) {
+    $(".navbar-collapse").removeClass("show");
+    const doc = document.documentElement;
+    let top = (window.pageYOffset || doc.scrollTop) - (doc.clientTop || 0);
+    let index = Math.floor(top / window.innerHeight);
 
-    var index = Math.floor(top / window.innerHeight);
-    var percent = (1 / window.innerHeight) * (top - window.innerHeight * index);
-    var percentEasing = easeInCubic(percent);
+    if (e.type === "click") {
+      e.preventDefault();
+      index = e.target.id;
+
+      switch (index) {
+        default:
+        case "1":
+          top = 687;
+          break;
+        case "2":
+          top = 1400;
+          break;
+        case "3":
+          top = 2175.5;
+          break;
+      }
+
+      $("html, body").animate(
+        {
+          scrollTop: top,
+        },
+        150
+      );
+    }
+
+    let percent = (1 / window.innerHeight) * (top - window.innerHeight * index);
+    const percentEasing = easeInCubic(percent);
 
     $("section").removeClass("active");
     $("section").eq(index).addClass("active");
@@ -37,7 +64,8 @@ document.addEventListener("DOMContentLoaded", function () {
     if (index != lastIndex) {
       lastIndex = index;
       sectionContainer.style.transform = "translate(0,0)";
-      console.log("activate: ", index);
+
+      console.log("activate now: ", index);
     }
 
     if (indexArray[index].horizontal === false) {
@@ -48,16 +76,26 @@ document.addEventListener("DOMContentLoaded", function () {
         "translate(-" + 50 * percentEasing + "%, 0)";
     }
 
-    if (window.pageYOffset >= 700) {
-      console.log("1", window.pageYOffset);
+    if (window.pageYOffset >= 660) {
       lockerContainer.style.position = "fixed";
       lockerContainer.style.visibility = "visible";
+      lockerContainer.style.opacity = "0.2";
+      lockerContainer.style.zIndex = "1";
+      if (window.pageYOffset >= 650) {
+        lockerContainer.style.opacity = "0.5";
+      }
+      if (window.pageYOffset >= 670) {
+        lockerContainer.style.opacity = "1";
+      }
+      if (window.pageYOffset >= 2222 && window.pageYOffset <= 2760) {
+        lockerContainer.style.zIndex = "-10000000";
+      }
     } else {
-      console.log("221", window.pageYOffset);
       lockerContainer.style.visibility = "hidden";
     }
   };
 
-  document.addEventListener("scroll", handleScroll);
-  document.addEventListener("gesturechange", handleScroll);
+  $(".locker > a").click(handleScroll);
+  $(document).scroll(handleScroll);
+  $(document).on("gesture_move", handleScroll);
 });
--- js/svg-animation.js
+++ js/svg-animation.js
@@ -1,16 +1,11 @@
-// Get the id of the <path> element and the length of <path>
 var triangle = document.getElementById("triangle");
 var length = triangle.getTotalLength();
-
 var sloganBlock = document.getElementById("slogan");
 
-// The start position of the drawing
 triangle.style.strokeDasharray = length;
 
-// Hide the triangle by offsetting dash. Remove this line to show the triangle before scroll draw
 triangle.style.strokeDashoffset = length;
 
-// Find scroll percentage on scroll (using cross-browser properties), and offset dash same amount as percentage scrolled
 window.addEventListener("scroll", drawLaptop);
 
 function drawLaptop() {
@@ -25,6 +20,5 @@ function drawLaptop() {
 
   var draw = length * scrollpercent * 3;
 
-  // Reverse the drawing (when scrolling upwards)
   triangle.style.strokeDashoffset = length - draw;
 }
